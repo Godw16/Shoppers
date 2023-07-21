98d9544fb6eb00e5b5fdf6877006e2c5637ea389
Godw16 <124239381+Godw16@users.noreply.github.com>
2023-07-21 12:32:35 +0530

    Create Login.js

---
+++ src/pages/Login.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+      
+    <><br/><br/><br/><br /><br/><div>
+      <div className='productContainer'>
+      <span>Log in</span>
+       
+      </div>
+
+    </div> <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/></>
+      
+    </div>
+  )
+}
+
+export default login
