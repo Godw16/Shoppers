bde5582407ceaf70b57bc43ab9d5b38d5ca03a9d
Godw16 <124239381+Godw16@users.noreply.github.com>
2023-07-21 12:32:16 +0530

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+const Home = () => {
+  return (
+    
+    <div>
+        <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      
+      <h3>Home</h3>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      
+    </div>
+  )
+}
+
+export default Home
